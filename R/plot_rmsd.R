4ac0d6482e007f2bbb45d905f454afec67a50c11
Hafeez Ur Rehman <reh.hafeez@gmail.com>
2026-01-25 17:51:18 +0800

    Update plot_rmsd.R

--- R/plot_rmsd.R
+++ R/plot_rmsd.R
@@ -4,12 +4,12 @@
 #' @param color Line color (default: "blue")
 #' @param title Plot title (default: "RMSD Analysis")
 #' @param show_stats Show statistics overlay (default: FALSE)
-#' @param xlab X-axis label (default: "Time (ps)")
+#' @param xlab X-axis label (default: "Time (ns)")
 #' @param ylab Y-axis label (default: "RMSD (nm)")
 #' @return ggplot2 object
 #' @export
 #' @importFrom ggplot2 ggplot aes geom_line labs theme_classic theme element_text annotate
-plot_rmsd <- function(data, color = "blue", title = "RMSD Analysis", show_stats = FALSE, xlab = "Time (ps)", ylab = "RMSD (nm)") {
+plot_rmsd <- function(data, color = "blue", title = "RMSD Analysis", show_stats = FALSE, xlab = "Time (ns)", ylab = "RMSD (nm)") {
   if (ncol(data) < 2) stop("Data must have at least 2 columns")
   colnames(data)[1:2] <- c("Time", "RMSD")
   p <- ggplot2::ggplot(data, ggplot2::aes(x = Time, y = RMSD)) +
